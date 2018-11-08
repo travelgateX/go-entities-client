fc3381e1b287cb04858317904300344c3baa7b43
Carlos Guzmán <cguzman@xmltravelgate.com>
2018-11-08 15:37:25 +0100

    Query blank functions has been added

--- entities/querys.go
+++ entities/querys.go
@@ -1,8 +1,42 @@
 package entities
 
-import "github.com/travelgateX/go-entities-client/model"
+import (
+	"errors"
+
+	"github.com/travelgateX/go-entities-client/model"
+)
 
 // Accesses Entities API query function
 func (c *Client) Accesses(id int) (model.AdminQuery, error) {
 	return c.NewQuery(accessesRQ(id))
+}
+
+// Suppliers Entities API query function
+func (c *Client) Suppliers() (model.AdminQuery, error) {
+	return model.AdminQuery{}, errors.New("not implemented")
+}
+
+// Clients Entities API query function
+func (c *Client) Clients() (model.AdminQuery, error) {
+	return model.AdminQuery{}, errors.New("not implemented")
+}
+
+// ServiceAPI Entities API query function
+func (c *Client) ServiceAPI() (model.AdminQuery, error) {
+	return model.AdminQuery{}, errors.New("not implemented")
+}
+
+// PointsOfSale Entities API query function
+func (c *Client) PointsOfSale() (model.AdminQuery, error) {
+	return model.AdminQuery{}, errors.New("not implemented")
+}
+
+// Profiles Entities API query function
+func (c *Client) Profiles() (model.AdminQuery, error) {
+	return model.AdminQuery{}, errors.New("not implemented")
+}
+
+// Entities Entities API query function
+func (c *Client) Entities() (model.AdminQuery, error) {
+	return model.AdminQuery{}, errors.New("not implemented")
 }
