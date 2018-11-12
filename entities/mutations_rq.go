ad9dc1e6ad92a698d59248697653e234ceaaa9a3
Carlos Guzmán <cguzman@xmltravelgate.com>
2018-11-12 12:24:48 +0100

    Rollback last commit

--- entities/mutations_rq.go
+++ entities/mutations_rq.go
@@ -1,16 +1,17 @@
 package entities
 
 import (
+	"strconv"
 	"strings"
 )
 
 // grantAccessToGroupRQ returns graphql request mutation
-func grantAccessToGroupRQ(code string, groups []string) string {
+func grantAccessToGroupRQ(id int, groups []string) string {
 	rq := `
 	  mutation{
 			admin{
 				grantAccessToGroup(input:{
-					id:$CODE$
+					id:$ID$
 					groups:$GROUPS$
 				}){
 					code
@@ -23,7 +24,7 @@ func grantAccessToGroupRQ(code string, groups []string) string {
 			}
 	  }    
 	`
-	rq = strings.Replace(rq, "$CODE$", code, 1)
+	rq = strings.Replace(rq, "$ID$", strconv.Itoa(id), 1)
 	rq = strings.Replace(rq, "$GROUPS$", sliceToQuotedStringFormat(groups), 1)
 	return rq
 }
